9b20fd88d2ec5a8c8d964f9222a0de6de79daae7
Sajad Khatiri <s.khatiri@gmail.com>
2022-08-30 15:45:55 +0200

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Aerialist",
+    name="aerialist",
     version="0.0.1",
     author="Sajad Khatiri",
     author_email="s.khatiri@gmail.com",
@@ -13,7 +13,19 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/skhatiri/Aerialist",
     project_urls={"Bug Tracker": "https://github.com/skhatiri/Aerialist/issues"},
-    license="MIT",
-    packages=["aerialist"],
-    install_requires=["requests"],
+    license="GNU GPLv3",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "similaritymeasures",
+        "matplotlib",
+        "pyulog",
+        "mavsdk",
+        "keyboard",
+        "python-decouple",
+        "csv-logger",
+        "statsmodels",
+        "webdavclient3",
+        "ruptures",
+        "shapely",
+    ],
 )
