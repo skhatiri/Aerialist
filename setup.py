ba67bb8cecfbf5f023718cef50c61301573e9719
Sajad Khatiri <s.khatiri@gmail.com>
2022-08-30 16:13:13 +0200

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aerialist",
-    version="0.0.2",
+    version="0.0.21",
     author="Sajad Khatiri",
     author_email="s.khatiri@gmail.com",
     description="UAV Test Bench",
