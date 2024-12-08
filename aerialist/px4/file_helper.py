f44ea6ac93651c153c1fffe604a4bcc9f023bd7e
skhatiri <skhatiri@anybotics.com>
2024-12-08 17:21:29 +0100

    make sure nextcloud dl folder exists

--- aerialist/px4/file_helper.py
+++ aerialist/px4/file_helper.py
@@ -216,6 +216,8 @@ def download(src_file: str, dest_path: str) -> str:
         cloud_path = get_webdav_path(src_file)
     dest_path += path.basename(cloud_path)
     try:
+        # Ensure the destination directory exists locally
+        os.makedirs(dest_path, exist_ok=True)
         webdav_client.download_file(cloud_path, dest_path)
         RETRIES = 0
     except RequestException as e:
