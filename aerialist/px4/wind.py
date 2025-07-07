6207a179225a50953df2a0b8355e75183a735451
AliJavadi <ali.fleissig@gmail.com>
2025-07-07 16:32:17 +0200

    This version has no errors and in my opinion it should work, but I don't observe the wind effect in uav plots. Maybe the reason is that the wind plugin is for gazeboo, and as I cannot run the gazeboo simulation on my machine, I don't see the wind effect on uav and its environment.

--- aerialist/px4/wind.py
+++ aerialist/px4/wind.py
@@ -2,17 +2,17 @@ from typing import NamedTuple, Tuple
 
 class Wind:
     class Params(NamedTuple):
-        mean_velocity: float
-        max_velocity: float
+        velocity_mean: float
+        velocity_max: float
         velocity_variance: float
-        direction_mean: Tuple[float, float, float]
+        direction: Tuple[float, float, float]
         direction_variance: float
         gust_start: float
         gust_duration: float
         gust_velocity_mean: float
         gust_velocity_max: float
         gust_velocity_variance: float
-        gust_direction_mean: Tuple[float, float, float]
+        gust_direction: Tuple[float, float, float]
         gust_direction_variance: float
 
     def __init__(self, params: Params) -> None:
@@ -21,16 +21,16 @@ class Wind:
     def to_dict(self):
         p = self.params
         return {
-            "mean_velocity": p.mean_velocity,
-            "max_velocity": p.max_velocity,
+            "velocity_mean": p.velocity_mean,
+            "velocity_max": p.velocity_max,
             "velocity_variance": p.velocity_variance,
-            "direction_mean": list(p.direction_mean),
+            "direction": list(p.direction),
             "direction_variance": p.direction_variance,
             "gust_start": p.gust_start,
             "gust_duration": p.gust_duration,
             "gust_velocity_mean": p.gust_velocity_mean,
             "gust_velocity_max": p.gust_velocity_max,
             "gust_velocity_variance": p.gust_velocity_variance,
-            "gust_direction_mean": list(p.gust_direction_mean),
+            "gust_direction": list(p.gust_direction),
             "gust_direction_variance": p.gust_direction_variance,
         }
